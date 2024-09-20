b6440baabdcd772b792988602a6284ea7c147f13
Akdodiya <aniket.dodiya@agami-tech.com>
2024-09-20 17:22:25 +0530

    Node adding added

--- src/views/IVR/componets/custom-components/CustomPanel.tsx
+++ src/views/IVR/componets/custom-components/CustomPanel.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { CatologControlData, IntegrationControlData, OperationControlData } from "../../configs/ControllerNodeData";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import { SvgIcon, Tooltip } from "@mui/material";
+import { GenralControlData } from "@/views/IVR/configs/ControllerNodeData";
 function CustomPanel() {
   const [isGeneralControlsOpen, setIsGeneralControlOpen] = useState(true);
   const [isAskQuestionOpen, setIsAskQuestion] = useState(false);
@@ -44,46 +44,20 @@ function CustomPanel() {
                         </SvgIcon>
                       )}
                     </div>
-
-                    {isGeneralControlsOpen && (
-                      <div className="custom-send-message p-2 flex flex-wrap justify-center cursor-move" onDragStart={(event) => onDragStart(event, "SendMessageNode")} draggable>
-                        <div className="bg-red-500 rounded-lg shadow-lg p-3">
-                          <div className="flex items-center">
-                            <div>
-                              <div className="text-white font-bold text-sm">Send a message</div>
-                              <div className="text-gray-900 text-xs">With no response required from visitor</div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )}
-
-                    {isGeneralControlsOpen && (
-                      <div className="custom-ask-question p-2 flex flex-wrap justify-center cursor-pointer" onClick={() => setIsAskQuestion(!isAskQuestionOpen)}>
-                        <div className="bg-orange-400 rounded-lg shadow-lg p-3">
-                          <div className="flex items-center">
-                            <div>
-                              <div className="text-white font-bold text-sm">Ask Question</div>
-                              <div className="text-gray-900 text-xs">Ask questions and store user Input in variable</div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )}
-
-                    {isGeneralControlsOpen && (
-                      <div className="custom-set-condition p-2 flex flex-wrap justify-center cursor-move" onDragStart={(event) => onDragStart(event, "setCondition")} draggable>
-                        <div className="bg-blue-500 rounded-lg shadow-lg p-3">
-                          <div className="flex items-center">
-                            <div>
-                              <div className="text-white font-bold text-sm">Set a condition</div>
-                              <div className="text-gray-900 text-xs">Send Message(s) based on logical(s) conditions</div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )}
                   </div>
+                  {isGeneralControlsOpen && (
+                    <div className="p-1 flex flex-wrap justify-center">
+                      {GenralControlData.map((controlData: any) => (
+                        <span key={controlData.id} className="m-2 cursor-move">
+                          <Tooltip title={`${controlData.name}`}>
+                            <div className={` border text-2xl text-gray-600 transition duration-300 transform hover:scale-110 `} onDragStart={(event: any) => onDragStart(event, controlData.type)} draggable>
+                              <SvgIcon className="h-[50px] w-[50px]">{controlData.svg}</SvgIcon>
+                            </div>
+                          </Tooltip>
+                        </span>
+                      ))}
+                    </div>
+                  )}
                 </div>
               </>
             )}
